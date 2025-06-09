e57439417b4d38cb72b533efb2c530b98789e5a8
luckfer7 <lucasferreira059@gmail.com>
2025-06-09 18:25:24 -0300

    topheader feito

--- src/components/TopHeader/index.jsx
+++ src/components/TopHeader/index.jsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
+
 export default function TopHeader() {
     return (
-        <div>
-            <p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <span>ShopNow</span> </p>
+        <div className=" bg-black flex justify-center py-4" >
+            <p className=" text-[#FAFAFA] font-raleway " >Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p> 
+            <Link className=" font-semibold underline text-[#FAFAFA] font-raleway ml-2 " href="/" >ShopNow</Link> 
         </div>
     )
 }
