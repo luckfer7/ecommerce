d7c7f73461ddf81900b8d179ceaa2726447c1183
luckfer7 <lucasferreira059@gmail.com>
2025-06-09 17:29:29 -0300

    continuação na utra branch

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import TopHeader from "@/components/TopHeader";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-
+      <TopHeader />
     </div>
   );
 }
---
+++ src/components/TopHeader/index.jsx
@@ -0,0 +1,7 @@
+export default function TopHeader() {
+    return (
+        <div>
+            <p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <span>ShopNow</span> </p>
+        </div>
+    )
+}
