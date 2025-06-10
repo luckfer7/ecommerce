189d3f3f085a66991d445923626e246f8bcacae3
luckfer7 <lucasferreira059@gmail.com>
2025-06-10 19:09:26 -0300

    componente botão criado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,28 +4,30 @@ import Container from "../Container";
 
 export default function Header() {
     return (
-        <header className="flex " >
+        <header className="flex justify-evenly " >
             Exclusive
             <nav className="flex " >
-                <ul className="flex" >
+                <ul className="flex gap-[48px] " >
                     <li> <Link href="/" >Home</Link> </li>
                     <li> <Link href="/contact" >Contact</Link> </li>
                     <li> <Link href=" /about " >About</Link> </li>
                     <li> <Link href=" /signup " >Sign Up</Link> </li>
                 </ul>
             </nav>
-
-            <Container> 
-                <input placeholder="what are you looking for?" /> 
-            </Container>
-            <Container>
-                <Container>
-                    <Image />
+            <Container className="flex gap-6">
+                <Container> 
+                    <input placeholder="what are you looking for?" /> 
                 </Container>
                 <Container>
-                    <Image />
+                    <Container>
+                        <Image />
+                    </Container>
+                    <Container>
+                        <Image />
+                    </Container>
                 </Container>
             </Container>
+            
         </header>
     )
 }
